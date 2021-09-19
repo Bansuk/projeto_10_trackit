4a83b74e86619d8e2b2bb30607944141de200dd9
Sidney Outeiro <sid@dcc.ufrj.br>
2021-09-19 07:39:30 -0300

    preparando para o review

--- src/globalStyles.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from "styled-components";
-
-const GlobalStyle = createGlobalStyle`
-    body{
-        background-color: #f2f2f2;
-    }
-`;
-
-export default GlobalStyle;
